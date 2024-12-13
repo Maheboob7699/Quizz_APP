6534439b44ecb38f49d9034e3f0ac8b0a4fb9be2
Maheboob7699 <murdimaheboob@gmail.com>
2024-12-13 01:23:20 -0900

    quizz

--- script.js
+++ script.js
@@ -586,18 +586,18 @@ const questionsAndAnswers = [
 ];
 
 let randomQuestion = [];
-let usedIndices = new Set();  
+let usedIndices = new Set();
 
 for (let i = 0; i < 10; i++) {
   let randomNumber = Math.floor(Math.random() * questionsAndAnswers.length);
-  
+
   if (usedIndices.has(randomNumber)) {
     i--;
 
   } else {
-    usedIndices.add(randomNumber); 
+    usedIndices.add(randomNumber);
     console.log("random number is", randomNumber);
-    
+
     let questandAns = {
       q: questionsAndAnswers[randomNumber].q,
       a: questionsAndAnswers[randomNumber].a,
@@ -842,61 +842,147 @@ function dashboardPage() {
 
 function admin() {
   let home = document.querySelector(".home");
+  let quizz = document.querySelector(".quizz-menu");
+  let quizzSection = document.querySelector(".quizz-section");
+  let quizzContainer = document.querySelector(".quizz-conatiner");
   let sideBar = document.querySelector(".bar");
   let adminStart = document.querySelector(".admin-start");
   let tableContainer = document.getElementById("table-container")
   let scoreRender = JSON.parse(localStorage.getItem("UserDetail")) || [];
+  let localQuizz = JSON.parse(localStorage.getItem("quizzData")) || [];
+
   let sortedDetail = scoreRender.sort((a, b) => b.score - a.score);
 
 
-sideBar.addEventListener("click",function(){
-  this.classList.add('rotate');
-  
-})
+  sideBar.addEventListener("click", function () {
+    this.classList.add('rotate');
+
+  })
 
   home.addEventListener("click", function () {
-    this.classList.add('clicked');
+    home.classList.add('clicked');
+    quizz.classList.remove('clicked');
+    tableContainer.style.display = "block";
     adminStart.style.display = "none";
+    quizzSection.style.display = "none";
+
+
+    if (tableContainer.querySelector("table")) {
+      return;
+    }
+
     let table = document.createElement("table");
-  let headerRow = document.createElement("tr");
-  let headers = ["Sr No.", "Name", "Email id", "Scores", "Action"];
-  headers.forEach(header => {
-    let th = document.createElement("th");
-    th.textContent = header;
-    headerRow.appendChild(th);
-  });
-  table.appendChild(headerRow);
+    let headerRow = document.createElement("tr");
+    let headers = ["Sr No.", "Name", "Email id", "Scores", "Action"];
+    headers.forEach(header => {
+      let th = document.createElement("th");
+      th.textContent = header;
+      headerRow.appendChild(th);
+    });
+    table.appendChild(headerRow);
 
-  sortedDetail.forEach((user, index) => {
-    let row = document.createElement("tr");
-    let tdSrNo = document.createElement("td");
-    tdSrNo.textContent = index + 1;
-    let tdName = document.createElement("td");
-    tdName.textContent = user.name;
-    let tdEmail = document.createElement("td");
-    tdEmail.textContent = user.email;
-    let tdScores = document.createElement("td");
-    tdScores.textContent = user.quizzScore;
-    let tdAction = document.createElement("td");
-    let actionButton = document.createElement("button");
-    actionButton.classList = "veiw-result-btn"
-    actionButton.textContent = "view result";
-    tdAction.appendChild(actionButton);
+    sortedDetail.forEach((user, index) => {
+      let row = document.createElement("tr");
+      let tdSrNo = document.createElement("td");
+      tdSrNo.textContent = index + 1;
+      let tdName = document.createElement("td");
+      tdName.textContent = user.name;
+      let tdEmail = document.createElement("td");
+      tdEmail.textContent = user.email;
+      let tdScores = document.createElement("td");
+      tdScores.textContent = user.quizzScore;
+      let tdAction = document.createElement("td");
+      let actionButton = document.createElement("button");
+      actionButton.classList = "veiw-result-btn"
+      actionButton.textContent = "view result";
+      tdAction.appendChild(actionButton);
 
-    row.appendChild(tdSrNo);
-    row.appendChild(tdName);
-    row.appendChild(tdEmail);
-    row.appendChild(tdScores);
-    row.appendChild(tdAction);
+      row.appendChild(tdSrNo);
+      row.appendChild(tdName);
+      row.appendChild(tdEmail);
+      row.appendChild(tdScores);
+      row.appendChild(tdAction);
 
-    table.appendChild(row);
+      table.appendChild(row);
+
+    })
+    tableContainer.appendChild(table);
+  })
+
+  quizz.addEventListener("click", function () {
+    quizz.classList.add('clicked');
+    home.classList.remove('clicked');
+
+    adminStart.style.display = "none";
+    tableContainer.style.display = "none";
+    quizzSection.style.display = "block";
+
+    if (quizzContainer.querySelector("table")) {
+      return;
+    }
+
+
+    let tableQuizz = document.createElement("table");
+    quizzRow = document.createElement("tr");
+    headers = ["Sr No", "Questions", "Actions"];
+    headers.forEach((header) => {
+      let headerRow = document.createElement("th");
+      headerRow.innerText = header;
+      quizzRow.appendChild(headerRow);
+    })
+
+    tableQuizz.appendChild(quizzRow);
+
+    localQuizz.forEach((quizzData, index) => {
+      let row = document.createElement("tr");
+
+      let tdSrNo = document.createElement("td");
+      tdSrNo.textContent = index + 1;
+
+      let tdName = document.createElement("td");
+      tdName.textContent = quizzData.q;
+
+      let tdAction = document.createElement("td");
+      let showButton = document.createElement("button");
+      showButton.classList = "show-quizz"
+      let showIcon = document.createElement("i");
+      showIcon.classList.add("fa-regular", "fa-eye");  
+      showButton.appendChild(showIcon);
+
+      let editButton = document.createElement("button");
+      editButton.classList = "edit-quizz";
+      let editIcon = document.createElement("i");
+       editIcon.classList.add("fa-solid", "fa-pencil");
+       editButton.appendChild(editIcon)
+
+      let deleteButton = document.createElement("button");
+      deleteButton.classList = "delete-quizz";
+      let deleteIcon = document.createElement("i");
+      deleteIcon.classList.add("fa-solid", "fa-trash-can");
+      deleteButton.appendChild(deleteIcon)
+      
+
+      tdAction.appendChild(showButton);
+      tdAction.appendChild(showButton);
+      tdAction.appendChild(editButton);
+      tdAction.appendChild(deleteButton);
+
+      row.appendChild(tdSrNo);
+      row.appendChild(tdName);
+      row.appendChild(tdAction);
+
+      tableQuizz.appendChild(row);
+    })
+
+    console.log(tableQuizz);
+    quizzContainer.appendChild(tableQuizz)
 
   })
-  tableContainer.appendChild(table);
-  })
 
 
-  
+
+
+
 }
 
 
